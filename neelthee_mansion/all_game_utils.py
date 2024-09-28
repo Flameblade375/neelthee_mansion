de0249388d585ec72c3b4e14396d558304afe3ee
Flamablade375 <alexander@xandy.rocks>
2024-09-28 12:19:54 +0100

    more bug fixes

--- neelthee_mansion/all_game_utils.py
+++ neelthee_mansion/all_game_utils.py
@@ -4,7 +4,7 @@ from re import *
 def ask_for_consent(text: str) -> bool:
     while True:
         type_text(f"{text}? Y/N")
-        anser = str(input(">")).lower()
+        anser = str(input(">")).lower().strip()
         if anser == 'y':
             return True
         elif anser == 'n':
